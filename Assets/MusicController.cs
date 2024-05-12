d1703d873d753a25435bdae18546c673a8fe2b09
Lewis Whiteman <konomira@gmail.com>
2024-05-12 21:19:57 +0100

    Added crossfade to music transition

--- Assets/MusicController.cs
+++ Assets/MusicController.cs
@@ -5,9 +5,10 @@ public class MusicController : MonoBehaviour
 {
     private static MusicController instance;
 
-    public AudioClip menuMusic;
-    public AudioClip gameMusic;
-    public AudioSource audioSource;
+    public AudioSource menuMusic;
+    public AudioSource gameMusic;
+
+    public float defaultVolume = 1.0f;  
 
     private void Awake()
     {
@@ -17,10 +18,6 @@ public class MusicController : MonoBehaviour
             instance = this;
     }
 
-    private void OnValidate()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
     public static void SwitchToGame()
     {
         instance.StartCoroutine(instance.Transition(instance.gameMusic));
@@ -31,15 +28,27 @@ public class MusicController : MonoBehaviour
         instance.StartCoroutine(instance.Transition(instance.menuMusic));
     }
 
-    private IEnumerator Transition(AudioClip clip)
+    private IEnumerator Transition(AudioSource newSource)
     {
-        var time = audioSource.time;
+        newSource.volume = 0f;
+        var oldSource = menuMusic == newSource ? gameMusic : menuMusic;
 
-        time %= clip.length;
+        var time = oldSource.time;
 
-        audioSource.clip = clip;
-        audioSource.Play();
-        audioSource.time = time;
+        time %= newSource.clip.length;
+        newSource.time = time;
+        float percentage = 0f;
+
+        while(oldSource.volume > 0)
+        {
+            oldSource.volume = Mathf.Lerp(defaultVolume, 0, percentage);
+            newSource.volume = Mathf.Lerp(0, defaultVolume, percentage);
+            percentage += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        newSource.volume = defaultVolume;
+        
         yield break;
     }
 }
